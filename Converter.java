30a84aee44a6301ad15ec5bbe64464a25e37fce1
EugenSnegirjow <EugenSnegirjow@yandex.ru>
2023-10-26 21:47:41 +0300

    Мой первый коммит

---
+++ Converter.java
@@ -0,0 +1,14 @@
+class Converter{
+	double USD;
+	double EUR;
+	double JPY;
+
+	public Converter(double USD, double EUR, double JPY){
+		this.USD = USD;
+		this.EUR = EUR;
+		this.JPY = JPY;
+
+		
+
+	}
+}
---
+++ Praktikum.java
@@ -0,0 +1,6 @@
+public class Praktikum{
+	public static void main(String[] args){
+		Converter converter = new Converter(93.00, 98.00, 0.73);
+		System.out.println("git commit -m Мой первый коммит.");
+	}
+}
